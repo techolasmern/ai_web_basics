bc17155e8a99a5322426e6dd1e7230d3785321c4
TECHOLAS MERN <techolas.vishnumk@gmail.com>
2026-08-14 11:43:32 +0530

    js - operators

---
+++ JavaScript/day2.js
@@ -0,0 +1,18 @@
+const a = 10;
+const b = 20;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** 3); // 10 * 10 * 10
+console.log(a ** 3);
+
+let num = 10;
+// const inc = num++; // post increment
+const inc = ++num; // pre increment
+console.log(inc, num);
+
+let n = 10;
+const d = --n;
+console.log(n, d);
