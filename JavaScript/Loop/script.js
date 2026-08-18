fc80c64a57c2408974b07919b769a8028fcfc5a2
TECHOLAS MERN <techolas.vishnumk@gmail.com>
2026-08-18 12:53:48 +0530

    loops questions

--- JavaScript/Loop/script.js
+++ JavaScript/Loop/script.js
@@ -25,3 +25,49 @@ do {
 } while (j <= 0);
 
 console.log("do while end");
+
+// patterns
+
+for (let i = 1; i <= 5; i++){
+    console.log("Inner Loop ok");
+    for (let j = 1; j <= 5; j++){
+        console.log(j); // 1, 2, 3, 4, 5
+    }
+    console.log("Outer loop end")
+}
+console.log("Final log")
+
+// questions
+
+const num = 11;
+
+let is_prime = true;
+for (let p = 2; p <= num / 2 ; p++){
+    if (num % p == 0) {
+        is_prime = false;
+        break;
+    }
+}
+
+if (is_prime) {
+    console.log("is prime");
+} else {
+    console.log("is not a prime");
+}
+
+// palindrome
+
+// 123 ->
+
+const pNumber = 123;
+
+let tempNumber = pNumber;
+let rev = 0;
+
+while (tempNumber > 0) {
+    const rem = tempNumber % 10;
+    rev = rev * 10 + rem;
+    tempNumber = parseInt(tempNumber / 10);
+}
+
+console.log(pNumber == rev);
