1e9334600420f082a9e87016304f4b79dd205ef0
TECHOLAS MERN <techolas.vishnumk@gmail.com>
2026-08-18 11:44:08 +0530

    loops

---
+++ JavaScript/Loop/script.js
@@ -0,0 +1,27 @@
+// Loops in JavaScript
+
+// for, while, do while, for in, for of
+// 1 - 10 print
+for (let i = 1; i <= 10; i++){
+    console.log(i); // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+}
+console.log("Loop end");
+
+// while
+
+let k = 1;
+while (k <= 10) {
+    console.log(k);
+    k++;
+}
+
+console.log("While end");
+
+let j = 1;
+
+do {
+    console.log(j);
+    j++;
+} while (j <= 0);
+
+console.log("do while end");
