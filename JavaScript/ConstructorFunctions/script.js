c7e15fea39aa14b9bbbb8bd60649c378cc13a245
TECHOLAS MERN <techolas.vishnumk@gmail.com>
2026-09-02 11:38:09 +0530

    constructor function - 2

--- JavaScript/ConstructorFunctions/script.js
+++ JavaScript/ConstructorFunctions/script.js
@@ -1,8 +1,21 @@
-function User(name, age) {
-    this.name = name;
+function User(name, age, email, number) {
+    this.user_name = name;
     this.age = age;
+    this.email = email;
+    this.number = number;
+    this.get_email = function () {
+        return this.email
+    }
 }
 
-const user = new User("Alex", 20);
+const user1 = new User("Alex", 20, "nKZwM@example.com", 1234567890);
+const user2 = new User("Amal", 25, "0Cm0Q@example.com", 9876543210);
 
-console.log(user);
+user1.age = 100;
+user1.dep = "IT";
+
+console.log(user1);
+console.log(user2);
+
+const em = user2.get_email();
+console.log(em);
