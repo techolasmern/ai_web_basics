6711b3e3f96ecd298c8f0a4ae72e2d4658f5e7a4
TECHOLAS MERN <techolas.vishnumk@gmail.com>
2026-09-09 12:07:05 +0530

    updated todo

--- JavaScript/Todo/app.js
+++ JavaScript/Todo/app.js
@@ -1,8 +1,10 @@
 const form = document.getElementById("todo_form");
 const error = document.getElementById("error");
 const todoList = document.getElementById("list");
+const counter = document.getElementById("counter");
 
 form.addEventListener("input", (event) => {
+    counter.innerText = document.getElementById("todo_input").value.length;
     error.innerText = "";
 })
 
@@ -22,15 +24,31 @@ form.addEventListener("submit", (event) => {
     }
 
     const li = document.createElement("li");
-    li.innerHTML = todo;
-    const button = document.createElement("button");
-    button.innerHTML = "Delete";
-    button.classList.add("btn");
-    button.addEventListener("click", () => {
+    li.innerHTML = "<div><b>Task:</b> " + todo + "</div><div><b>Time:</b> " + new Date().toLocaleString() + "</div>";
+    li.classList.add("todo_item");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.classList.add("del-btn");
+    deleteButton.addEventListener("click", () => {
         li.remove();
     })
-    li.appendChild(button);
+
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    editButton.classList.add("ed-btn");
+    editButton.addEventListener("click", () => {
+        alert("Edit not available");
+    })
+
+    const btnContainer = document.createElement("div");
+    btnContainer.appendChild(deleteButton);
+    btnContainer.appendChild(editButton);
+    btnContainer.classList.add("btn-container");
+
+    li.appendChild(btnContainer);
     todoList.appendChild(li);
 
-    document.getElementById("todo_input").value = "";
+    counter.innerText = 0;
+    todoElement.value = "";
 })
